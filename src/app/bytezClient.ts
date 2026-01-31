11b74f3d7d0964153e1da88dc3ed0d05710f293f
gamingvninja <gamingvninja@gmail.com>
2026-01-31 17:00:45 +0000

    fuyhegiuwebfhiwvbiwhvwrvwvwrwrbrwbb

--- src/app/bytezClient.ts
+++ src/app/bytezClient.ts
@@ -1,41 +1,14 @@
+/* npm i bytez.js || yarn add bytez.js */
 
-import Bytez from "bytez.js";
+import Bytez from "bytez.js"
 
-const key = "2622dd06541127bea7641c3ad0ed8859";
-const sdk = new Bytez(key);
+const key = "2622dd06541127bea7641c3ad0ed8859" 
+const sdk = new Bytez(key)
 
-export async function runFluxDev(prompt: string) {
-    const model = sdk.model("black-forest-labs/FLUX.1-dev");
-    const { error, output } = await model.run(prompt);
-    if (error) {
-        throw new Error(error);
-    }
-    return output;
-}
+// choose Qwen3-0.6B 
+const model = sdk.model("Qwen/Qwen3-0.6B")
 
-export async function runClaudeOpus4_5(prompt: string) {
-    const model = sdk.model("anthropic/claude-3-opus");
-    const { error, output } = await model.run(prompt);
-    if (error) {
-        throw new Error(error);
-    }
-    return output;
-}
+// send input to model 
+const { error, output } = await model.run([ { "role": "user", "content": "Hello" } ])
 
-export async function runDeepseekOCR(prompt: string) {
-    const model = sdk.model("deepseek/deepseek-ocr");
-    const { error, output } = await model.run(prompt);
-    if (error) {
-        throw new Error(error);
-    }
-    return output;
-}
-
-export async function runSora2Pro(prompt: string) {
-    const model = sdk.model("sora/sora-2-pro");
-    const { error, output } = await model.run(prompt);
-    if (error) {
-        throw new Error(error);
-    }
-    return output;
-}
+console.log({ error, output });
